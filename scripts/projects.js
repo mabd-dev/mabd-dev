64d6c6e95e401674e1f8ad2e0a41fcd2432a79d0
MABD <mabd.universe@gmail.com>
2026-03-24 07:47:41 +0200

    projects: updated `view projec` icon

--- scripts/projects.js
+++ scripts/projects.js
@@ -135,8 +135,8 @@ function createProjectCard(project) {
             </div>
             <a href="${project.link}" class="project-link" ${project.link.startsWith('http') ? 'target="_blank"' : ''}>
                 View Project
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                 </svg>
             </a>
         </div>
